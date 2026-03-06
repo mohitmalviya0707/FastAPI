5b86fdbc4fa4eef5152d5ba510d8d1be017ab69d
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2026-03-06 19:49:00 +0530

    Update 1_pydantic_why.py

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -2,3 +2,14 @@ from pydantic import BaseModel, EmailStr, AnyUrl, Field
 from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
+  
+    name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Give the name of the patient in less than 50 chars', examples=['Nitish', 'Amit'])]
+    email: EmailStr
+    linkedin_url: AnyUrl
+    age: int = Field(gt=0, lt=120)
+    weight: Annotated[float, Field(gt=0, strict=True)]
+    married: Annotated[bool, Field(default=None, description='Is the patient married or not')]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
+    contact_details: Dict[str, str]
+
+
