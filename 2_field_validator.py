76472beee8c167713f81a780acba9c68135756f6
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2026-03-07 09:47:14 +0530

    Update 2_field_validator.py

--- 2_field_validator.py
+++ 2_field_validator.py
@@ -1,2 +1,12 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 from typing import List, Dict, Optional, Annotated
+
+class Patient(BaseModel):
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
