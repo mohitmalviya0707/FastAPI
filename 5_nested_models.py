aacbc2ba8cfa9a5505ca3da8e23f3f8ccc859ccd
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2026-03-10 09:36:10 +0530

    Update 5_nested_models.py

--- 5_nested_models.py
+++ 5_nested_models.py
@@ -1,16 +1,2 @@
-from pydantic import BaseModel
-class Address(BaseModel):
 
-    city: str
-    state: str
-    pin: str
-
-class Patient(BaseModel):
-
-    name: str
-    gender: str
-    age: int
-    address: Address
-
-address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
 
