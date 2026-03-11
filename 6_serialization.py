cf2e881cd31eb1003a9d7a26db1d602e715f9c83
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-11 08:45:31 +0530

    Update 6_serialization.py

--- 6_serialization.py
+++ 6_serialization.py
@@ -16,4 +16,12 @@ class Patient(BaseModel):
 address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
 
 address1 = Address(**address_dict)
+patient_dict = {'name': 'nitish', 'age': 35, 'address': address1}
+
+patient1 = Patient(**patient_dict)
+
+temp = patient1.model_dump(exclude_unset=True)
+
+print(temp)
+print(type(temp))
 
