aee121538addb1cdc507ca2985281855ec46b770
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-11 08:45:01 +0530

    Update 6_serialization.py

--- 6_serialization.py
+++ 6_serialization.py
@@ -1,3 +1,15 @@
 from pydantic import BaseModel
 
 class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class Patient(BaseModel):
+
+    name: str
+    gender: str = 'Male'
+    age: int
+    address: Address
+
