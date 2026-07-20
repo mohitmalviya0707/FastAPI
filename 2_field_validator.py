045a71857a3928d7fcefedb5be4527ab410c276c
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-07-20 07:39:27 +0530

    Fix indentation in Patient class definition

--- 2_field_validator.py
+++ 2_field_validator.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 from typing import List, Dict, Optional, Annotated
 
-class Patient(BaseModel):          ..
+class Patient(BaseModel):          
 
     name: str  
     email: EmailStr
